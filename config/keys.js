d896fec6ce873ec3ccbb976ed76c5df28cbdcf75
Hector Beltran <HectorBeltran@Hectors-MacBook-Pro.local>
2018-06-20 15:06:10 -0700

    started UI

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://hacktor:hacktor@ds263460.mlab.com:63460/storybooks-dev-hacktor',
+  mongoURI: 'mongodb://hacktor:hacktor1@ds263460.mlab.com:63460/storybooks-dev-hacktor',
   googleClientID:'451060058900-g4bb2rkhf5t40nr5dfvfocnaobnhhpgq.apps.googleusercontent.com',
   googleClientSecret:'vj2ISHPQnhxYZHa8ZFDqaHbB'
 }
